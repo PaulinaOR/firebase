aa959b79e3a81c1876db4f3527c8854fbffe8215
Paulina <paulina.341822099@ucaldas.edu.co>
2020-05-24 21:01:03 -0500

    no pude hacer todo

---
+++ package.js
@@ -0,0 +1,64 @@
+//UI
+const inputTask = document.querySelector('#taskInput');
+const taskUl = document.querySelector('#taskContainer');
+//DB
+connectDB();
+const database = firebase.database();
+const taskRef = database.ref('task');
+taskRef.on('value', paintTask);
+deleteTask('value');
+update();
+
+function connectDB(){
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDI_E5H23Gz4T3YL2vQVc9Qjg7cbIIf-4E",
+    authDomain: "todolist2-e8916.firebaseapp.com",
+    databaseURL: "https://todolist2-e8916.firebaseio.com",
+    projectId: "todolist2-e8916",
+    storageBucket: "todolist2-e8916.appspot.com",
+    messagingSenderId: "135950247653",
+    appId: "1:135950247653:web:3ff34cc1e1af6f227ed60c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
+function addTask(){
+ const name = inputTask.value;
+   taskRef.push({
+      name: name,
+      IsComplete: false
+    })
+inputTask.value = '';
+}
+
+function update(){
+   taskRef.update({
+   "-M87TjRPo-2RLrmWb-ZL" : {
+   name : "nuevo valor 797",
+   isCopleted: false
+   }
+   }) 
+}
+
+function changeTask(event, i){
+  todoItems[i].isSelected = event.target.checked; 
+  updateUI();
+}
+
+function deleteTask(data){
+		todoItems.splice(data, 1);
+    updateUI();
+}
+
+function paintTask(data){
+//console.log(data.val());
+const result = data.val();
+let taskli = ""
+for(key in result){
+   const task = result[key];
+   taskli += `<li>${task.name}</li>`;
+}
+taskUl.innerHTML = taskli;
+}
